a346ea02c5e8a08d4ef9c71a43dc6ac94a18239d
Shane Rogers <rogerssh@tcd.ie>
2013-05-28 13:50:52 -0600

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socrada::Application.config.secret_token = '010acdd46029b84d38243ce80477dbed57fd16e2c05b640af9aeabd6a595575bc97f3182e55b210c2f90533360203db95f596435057e4aa7a60a2efb9c88eef5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Socrada::Application.config.session_store :cookie_store, key: '_socrada_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Socrada::Application.config.session_store :active_record_store
