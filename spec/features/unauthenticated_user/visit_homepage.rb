7a1b46982fafb195f016f673474b44aa498036ac
Shane Rogers <rogerssh@tcd.ie>
2013-05-29 14:11:35 -0600

    added specs and wireframing for the main hompeage

--- spec/features/unauthenticated_user/visit_homepage.rb
+++ spec/features/unauthenticated_user/visit_homepage.rb
@@ -3,17 +3,19 @@ require 'spec_helper'
 feature 'unauthenticated user can visit the hompeage' do
 
   context "unauthenticated user visits the root_url" do
-    it "root url is the homepage" do
+
+    it "displays the cover title" do
       visit root_url
-      expect(current_path).to eq home_url
+      within(:css, 'div#title'){
+        expect(page).to have_content("Socrada")
+      }
     end
 
-    xit "displays the cover copy" do
+    it "has a twitter login button" do
       visit root_url
-      expect(page).to have_content("Social Relationship Data Visualised")
-    end
-
-    xit "has a twitter login button" do
+      within(:css, 'div#central-login'){
+        expect(page).to have_css('a#twitter-login')
+      }
     end
 
     xit "twitter login button authenticates and redirects to the users profile" do
