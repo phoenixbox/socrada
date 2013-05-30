f94bac87819724a395fd0b81d8c7e91600cd48b9
Shane Rogers <rogerssh@tcd.ie>
2013-05-29 20:05:19 -0600

    resolved missing provider column in user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130530003320) do
+ActiveRecord::Schema.define(:version => 20130530020348) do
 
   create_table "users", :force => true do |t|
     t.string   "uid"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130530003320) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "image_url"
+    t.string   "provider"
   end
 
 end
