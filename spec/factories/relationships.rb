381c4bfd65d3684f595449bf14148d76e55b32ae
Shane Rogers <rogerssh@tcd.ie>
2013-06-01 23:21:10 -0600

    untracked the deleted files

--- spec/factories/relationships.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :relationship do
-    friends ""
-    followers ""
-    mutual ""
-  end
-end
